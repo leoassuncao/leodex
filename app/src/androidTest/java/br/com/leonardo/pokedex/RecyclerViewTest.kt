14c479f5feb78a4f4c95b8177393b2b748376ae6
Leonardo Assunção <leonardo.assuncao@aisdigital.com.br>
2021-02-24 10:24:02 -0300

    ädd test to recyclerview

--- app/src/androidTest/java/br/com/leonardo/pokedex/RecyclerViewTest.kt
+++
@@ -1,6 +0,0 @@
-package br.com.leonardo.pokedex
-
-/**
- * @author Leonardo Assunção
- * @since 23/02/21
- **/
--- app/src/androidTest/java/br/com/leonardo/pokedex/flow/pokemonList/MainActivityTest.kt
+++ app/src/androidTest/java/br/com/leonardo/pokedex/flow/pokemonList/MainActivityTest.kt
@@ -7,9 +7,14 @@ package br.com.leonardo.pokedex.flow.pokemonList
 
 
 import android.os.SystemClock
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
 import br.com.leonardo.pokedex.CustomAssertions.Companion.hasItemCount
 import br.com.leonardo.pokedex.CustomMatchers.Companion.withItemCount
@@ -34,5 +39,12 @@ class MainActivityTest {
         SystemClock.sleep(2000)
         onView(withId(R.id.rv_pokemons))
             .check(hasItemCount(964))
+    }
+
+    @Test
+    fun onClickRecyclerView() {
+        SystemClock.sleep(2000)
+        onView(withId(R.id.rv_pokemons)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(1, ViewActions.click()))
+        Espresso.pressBack()
     }
 }
